4625d17f2ddafcb532d1fb19e5c7a865d8f71ff0
ahmed-zaafan <ahmedmahmoudzeafan@gmail.com>
2021-09-16 20:24:31 +0200

    second edit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
+let open = document.getElementById('open-icon');
+
+
+
+$(document).ready(function(){
+    $('.open-icon , .close-icon ,.menu ,.hide').click(function(){
+        $('.open-icon').fadeToggle();
+        $('.close-icon').fadeToggle();
+        $('.menu').fadeToggle();
+        $('.hide').fadeToggle();
+    })
+})
+
 $(window).scroll(function(){
     x = $(window).scrollTop();
     if(x > 550){
         $('.btn5').css("opacity" , "1")
-        $('.navbar').css("background" , "#fff")
+        $('.home').css("background" , "#fff")
     }else{
         $('.btn5').css("opacity" , "0")
-        $('.navbar').css("background" , "gray")
+        $('.home').css("background" , "#ddd")
     }
     console.log(x);
 })
 
+new WOW().init();
+var scroll = new SmoothScroll('a[href*="#"]');
+ /** $(function() {  
+    $("body").niceScroll({
+        cursorcolor : "pink"
+    });
+}); **/
